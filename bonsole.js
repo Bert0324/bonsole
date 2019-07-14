10d95ac95bffca560700d46e1715c8b426973616
Bert <yuchenhuang0324@gmail.com>
2019-07-13 23:46:59 -0700

    1.0.2

--- bonsole.js
+++ bonsole.js
@@ -1,49 +1,85 @@
-const http = require('http');
 const open = require('open');
-const detectPort = require('detect-port');
-const ip = require('ip');
+const ip = require('ip').address();
+const fs = require('fs');
+const path = require('path');
+
+const event = {
+    notStart:Symbol('notStart'),
+    inStart:Symbol('inStart'),
+    started:Symbol('started')
+};
+let status = event.notStart;
+let messageQueue = [];
+let socket;
+
+const server = require('http').createServer((req, res)=>{
+    let html = `<!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>bonsole</title>
+                        <script src="build/index.js"></script>
+                    </head>
+                    <body>
+                    </body>
+                </html>`;
+    if (req.url.endsWith('.js')){
+        fs.readFile(path.resolve(__dirname, 'build/index.js'), (err, data)=>{
+            if (err){
+                throw new Error(err.toString());
+            } else {
+                res.writeHead(200, {"Content-Type":"application/javascript"});
+                res.end(data);
+            }
+        })
+    } else {
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+        res.end(html);
+    }
+});
+
+
+const io = require('socket.io')(server);
+io.on('connection', (so)=>{
+
+    socket = so;
+    status = event.started;
+
+    socket.on('ready', ()=>{
+        socket.emit('first-push',messageQueue);
+    });
+
+
+});
+
+
+
 
 module.exports = (content, port, option) => {
     if (Object.prototype.toString.call(port) !== "[object Number]"){
         option = port;
         port = 9094;
     }
-    detectPort(port, (err, _port)=>{
-        if (!err && port === _port){
-            try {
-                content = JSON.stringify(content);
-            } catch (e) {
-                throw new Error(`transfer to string fail: ${e}`);
-            }
-            let html = `<!DOCTYPE html><html>
-                                <head>
-                                    <meta charset="UTF-8">
-                                    <title>bonsole</title>
-                                    <script>
-                                        console.log(${content});
-                                    </script>
-                                </head>
-                                <body>
-                                    <pre>${content}</pre>
-                                </body>
-                            </html>`;
-            http.createServer((req, res)=>{
-                res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
-                res.end(html);
-            }).listen(port, ip.address(), ()=>{
+    switch (status) {
+        case event.notStart:
+            status = event.inStart;
+            messageQueue.push(content);
+            server.listen(port, ip, ()=>{
                 (async ()=>{
-                    await open(`http://${ip.address()}:${port}`, option);
-                    console.log(`console.log in http://${ip.address()}:${port}`);
+                    await open(`http://${ip}:${port}`, option);
+                    console.log(`console.log in http://${ip}:${port}`);
                 })();
             });
-        } else {
-            if (err){
-                console.log(err);
-            } else {
-                console.log(`port ${_port} in use, please set a not used port`);
-            }
-        }
-    });
+            break;
+        case event.inStart:
+            messageQueue.push(content);
+            break;
+        case event.started:
+            socket.emit('push', content);
+            break;
+        default:
+            break;
+    }
 };
 
 
